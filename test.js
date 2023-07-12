64b4701688ac1c2d26d7f260d40c804dabb002a2
kontb2a22 <ahmedxd.2009@gmail.com>
2023-07-12 04:05:26 +0300

    project skeleton complete

---
+++ test.js
@@ -0,0 +1,57 @@
+import express from "express";
+import { SerialPort } from "serialport";
+import { ReadlineParser } from "@serialport/parser-readline";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT;
+app.use(express.json({ extended: false }));
+
+const SERIALPORT = new SerialPort({
+  path: "COM15",
+  baudRate: 115200,
+});
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  /* options */
+});
+
+const io = socketIo(server,{ 
+    cors: {
+      origin: ‘http://localhost:3000'
+    }
+}) //in case server and client run on different urls
+
+io.on("connection", (socket) => {
+  const parser = SERIALPORT.pipe(new ReadlineParser({ delimiter: "\n" }));
+  parser.on("data", (data) => {
+    console.log(data);
+  });
+});
+
+app.get("/api/getSerialData", (req, res) => {
+  try {
+    const parser = SERIALPORT.pipe(new ReadlineParser({ delimiter: "\n" }));
+    parser.on("data", (data) => {
+      res.json(data);
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/api/listPorts", async (req, res) => {
+  try {
+    const serialPorts = await SerialPort.list();
+    res.json(serialPorts);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(6001, "localhost", () => {
+  console.log(`Server listening on port ${PORT}`);
+});
